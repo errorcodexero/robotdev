7cc5793d2a9d9860777a24f1340e69b8b0f955c5
Jack (Butch) Griffin <butch@comcast.net>
2018-01-20 16:50:56 -0800

    changed the make files so if one library failed to build, the build stops right there

--- makefiles/toplevel.mk
+++ makefiles/toplevel.mk
@@ -7,22 +7,28 @@ include ../../makefiles/valid.mk
 all:: tests robotsw
 
 tests::
-	for program in $(LIBSUBDIRS) $(TOPDIR) ; do \
+	@for program in $(LIBSUBDIRS) $(TOPDIR) ; do \
 		cd $$program ; \
 		make CONFIG=$(CONFIG) tests ; \
+		if [ $$? -ne 0 ]; then \
+			break ; \
+		fi \
 		cd .. ; \
 	done
 
 robotsw::
-	for program in $(LIBSUBDIRS) $(TOPDIR) ; do \
+	@for program in $(LIBSUBDIRS) $(TOPDIR) ; do \
 		cd $$program ; \
 		make CONFIG=$(CONFIG)  ; \
+		if [ $$? -ne 0 ]; then \
+			break ; \
+		fi ; \
 		cd .. ; \
 	done
 
 
 clean::
-	for program in $(LIBSUBDIRS) $(TOPDIR) ; do \
+	@for program in $(LIBSUBDIRS) $(TOPDIR) ; do \
 		cd $$program ; \
 		make CONFIG=$(CONFIG) clean ; \
 		cd .. ; \
