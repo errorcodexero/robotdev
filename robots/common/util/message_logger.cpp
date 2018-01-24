1679233f94cb912774088a03f14107580639c66f
Vi Vuong <vuongkyvi@gmail.com>
2018-01-24 15:33:34 -0800

    Added test function to logger

--- robots/common/util/message_logger.cpp
+++ robots/common/util/message_logger.cpp
@@ -14,32 +14,118 @@ messageLogger::messageLogger() {
 	mMessages.insert(std::pair<messageType, std::string>(messageType::error, ""));
 }
 
+bool messageLogger::getModeState(const messageType &type) const {
+	return mEnabledModes.at(type);
+}
+std::string messageLogger::getMessage(const messageType &type) const {
+	return mMessages.at(type);
+}
 bool messageLogger::enable(const messageType &type) {
 	mEnabledModes.at(type) = true;
 	return true;
 }
+bool messageLogger::disable(const messageType &type) {
+	mEnabledModes.at(type) = false;
+	return true;
+}
 
-
-bool messageLogger::append(const std::string &message, const messageType& type) {
+bool messageLogger::writeMsg(const std::string &message, const messageType& type, bool overwrite) {
 	bool r = mEnabledModes.at(type);
-	if (r) mMessages.at(type).append(message);
+	if (r)
+		if (!overwrite) mMessages.at(type).append(message);
+		else mMessages.at(type) = message;
 	return r;
 }
-bool messageLogger::append(double message, const messageType& type) {
+bool messageLogger::writeMsg(double message, const messageType& type, bool overwrite) {
 	bool r = mEnabledModes.at(type);
-	if (r) mMessages.at(type).append(std::to_string(message));
+	if (r) 
+		if (!overwrite) mMessages.at(type).append(std::to_string(message));
+		else mMessages.at(type) = std::to_string(message);
 	return r;
 }
-bool messageLogger::append(int message, const messageType& type) {
+bool messageLogger::writeMsg(int message, const messageType& type, bool overwrite) {
 	bool r = mEnabledModes.at(type);
-	if (r) mMessages.at(type).append(std::to_string(message));
+	if (r)
+		if (!overwrite) mMessages.at(type).append(std::to_string(message));
+		else mMessages.at(type) = std::to_string(message);
+		return r;
 	return r;
 }
-bool messageLogger::endMessage(std::ostream &f, messageType type, bool addEndl = true) {
+bool messageLogger::streamMsg(std::ostream &f, messageType type, bool clearMsg, bool addEndl = true) {
 	if (addEndl) {
 		f << mMessages.at(type) << std::endl;
 	}
 	else
 		f << mMessages.at(type);
+	if (clearMsg) mMessages.at(type) = "";
 	return true;
 }
+
+
+#ifdef MESSAGE_LOGGER_TEST
+
+void test_logger() {
+	messageLogger logger;
+	//Test enable function
+	logger.enable(messageType::debug);
+	logger.enable(messageType::warning);
+	logger.enable(messageType::error);
+	logger.enable(messageType::info);
+	
+	assert(logger.getModeState(messageType::debug));
+	assert(logger.getModeState(messageType::warning));
+	assert(logger.getModeState(messageType::info));
+	assert(logger.getModeState(messageType::error));
+	
+	//Test disable function
+	logger.disable(messageType::info);
+	
+	assert(!logger.getModeState(messageType::info));
+	assert(logger.writeMsg("Error ", messageType::error, false));
+	assert(logger.writeMsg("Debug ", messageType::debug, false));
+	assert(logger.writeMsg("Warning ", messageType::warning, false));
+	assert(!logger.writeMsg("Info ", messageType::info, false));
+
+	//Test writeMsg
+	assert(logger.writeMsg("message: ", messageType::error, false));
+	assert(logger.writeMsg("message: ", messageType::debug, false));
+	assert(logger.writeMsg("message: ", messageType::warning, false));
+	assert(!logger.writeMsg("message: ", messageType::info, false));
+
+	int errorNumber = -7, debugNumber = 12;
+	double warningNumber = -0.824;
+
+	assert(logger.writeMsg(errorNumber, messageType::error, false));
+	assert(logger.writeMsg(debugNumber, messageType::debug, false));
+	assert(logger.writeMsg(warningNumber, messageType::warning, false));
+	assert(!logger.writeMsg(-912.5, messageType::info, false));
+
+	assert(logger.getMessage(messageType::error) == "Error message: -7");
+	assert(logger.getMessage(messageType::debug) == "Debug message: 12");
+	assert(logger.getMessage(messageType::warning) == "Warning message: -0.824000");
+	assert(logger.getMessage(messageType::info) == "");
+
+	//Test overwriting
+	assert(logger.writeMsg("Overwrited error number: ", messageType::error, true));
+	assert(logger.writeMsg("Overwrited debug number: ", messageType::debug, true));
+	assert(logger.writeMsg("Overwrited warning number: ", messageType::warning, true));
+	assert(!logger.writeMsg("Overwrited info number: ", messageType::info, true));
+
+	assert(logger.writeMsg(errorNumber, messageType::error, false));
+	assert(logger.writeMsg(debugNumber, messageType::debug, false));
+	assert(logger.writeMsg(warningNumber, messageType::warning, false));
+	assert(!logger.writeMsg(-912.5, messageType::info, false));
+
+	assert(logger.getMessage(messageType::error) == "Overwrited error number: -7");
+	assert(logger.getMessage(messageType::debug) == "Overwrited debug number: 12");
+	assert(logger.getMessage(messageType::warning) == "Overwrited warning number: -0.824000");
+	assert(logger.getMessage(messageType::info) == "");
+
+
+}
+
+int main() {
+	test_logger();
+	return 0;
+}
+#endif // MESSAGE_LOGGER_TEST
--- robots/common/util/message_logger.h
+++ robots/common/util/message_logger.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <cassert>
 
 
 enum class messageType { debug, info, warning, error };
@@ -9,11 +10,14 @@ enum class messageType { debug, info, warning, error };
 class messageLogger {
 public:
 	messageLogger();
+	bool getModeState(const messageType&) const;
+	std::string getMessage(const messageType&) const;
 	bool enable(const messageType&);					//Enable a message type. Only enabled message types can be streamed
-	bool append(const std::string &message, const messageType&);		//Append to the message of the selected type
-	bool append(double message, const messageType&);					//Append to the message of the selected type
-	bool append(int message, const messageType&);						//Append to the message of the selected type
-	bool endMessage(std::ostream &f, messageType type, bool addEndln); //Stream message of a type
+	bool disable(const messageType&);
+	bool writeMsg(const std::string &message, const messageType&, bool overwrite);		//Append to the message of the selected type
+	bool writeMsg(double message, const messageType&, bool overwrite);					//Append to the message of the selected type
+	bool writeMsg(int message, const messageType&, bool overwrite);						//Append to the message of the selected type
+	bool streamMsg(std::ostream &f, messageType type, bool addEndln, bool clearMsg); //Stream message of a type
 private:
 	std::map<messageType, bool> mEnabledModes;
 	std::map <messageType, std::string> mMessages;
