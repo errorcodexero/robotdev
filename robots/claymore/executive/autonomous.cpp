b20f2a1c10374858231d5d2455215424fedf2728
Jack (Butch) Griffin <butch@comcast.net>
2018-02-11 12:15:12 -0800

    updated the autonomous code to make it more readable

--- robots/claymore/executive/autonomous.cpp
+++ robots/claymore/executive/autonomous.cpp
@@ -1,169 +1,354 @@
 #include "autonomous.h"
-
 #include "teleop.h" 
 #include "chain.h"
 #include "step.h"
+#include "message_logger.h"
 
-using namespace std; 
+using namespace std;
 
-double deg_to_rad(double deg){
-	return deg/180*PI;
-}
+//
+// An auto mode program that drives straight to score on the
+// switch on the same side as the robot
+//
+Executive same_switch
+{
+    Chain
+    {
+	vector<Step>
+	{
+	    Step
+	    {
+		Navx_drive_straight{128.0}
+	    },
+		
+            //
+	    // TODO: add grabber step to spit out the cube
+	    //
+	},
+	Executive
+	{
+	    Teleop()
+	}
+    }
+};
 
-const Inch BUMPER_LENGTH = 3.0;//inches thickness of one bumper
-
-const Inch ROBOT_LENGTH = 28.0 + 1 * BUMPER_LENGTH; //inches from front to back //TODO: change to match number of sides with bumpers
+//
+// An auto mode program that drives straight, turns left, drives straight, turns right
+// drives straight and deposits a cube on the switch on the opposite side from the starting
+// side of the robot
+//
+Executive opposite_switch
+{
+    Chain
+    {
+	vector<Step>
+	{
+	    Step
+	    {
+		Navx_drive_straight{52.0}
+	    },
+	    Step
+	    {
+		Wait{1.0}
+	    },
+	    Step
+	    {
+		Navx_rotate{-90.0}
+	    },
+	    Step
+	    {
+		Wait{1.0}
+	    },
+	    Step
+	    {
+		Navx_drive_straight{100.0}
+	    },
+	    Step
+	    {
+		Wait{1.0}
+	    },
+	    Step
+	    {
+		Navx_rotate{90.0}
+	    },
+	    Step
+	    {
+		Wait{1.0}
+	    },
+	    Step
+	    {
+		Navx_drive_straight{56}
+	    }
+	    //
+	    // TODO: add grabber step to spit out the cube
+	    //
+	},
+	Executive
+	{
+	    Teleop()
+	}
+    }
+};
+	
 
 Executive make_test_step(auto a){
-	return Executive{Chain{
-		Step{a},
-		Executive{Teleop{}}
-	}};
+    return
+	Executive
+        {
+	    Chain{
+		Step
+		{
+		    a
+		},
+		Executive
+		{
+		    Teleop
+		    {
+		    }
+		}
+	    }
+	};
 }
 
-Executive get_auto_mode(Next_mode_info info){
-	if(!info.autonomous) return Executive{Teleop()};
+Executive get_auto_mode(Next_mode_info info)
+{
+    messageLogger &logger = messageLogger::get() ;
+    
+    if(!info.autonomous)
+    {
+	logger.startMessage(messageLogger::messageType::info) ;
+	logger << "get_auto_mode - not in autonomous, returning Teleop mode" ;
+	logger.endMessage() ;
+	return Executive{Teleop()};
+    }
 
-	////////////////////////////
-	//
-	// Parts of other autonomous modes
-	// 
-
-	//////////////////////////
-	//
-	// Full autonomous modes
-	//
+#if AUTOMODE_TEST == 0
+    //
+    // AUTOMODE_TEST == 0, null automode test program
+    //
+    logger.startMessage(messageLogger::messageType::info) ;
+    logger << "get_auto_mode - AUTOMODE_TEST == 0, do nothing" ;
+    logger.endMessage() ;
+    
+    const Executive auto_program
+    {
+	Teleop{}
+    };
 	
-	//Auto mode which does nothing
-	const Executive auto_null{Teleop{}};
-
-	Executive motion_profile_test{Chain{
-		Step{MP_drive{100.0}}, //drive 100 inches
-		Executive{Teleop{}}	
-	}};
-
-	Executive drive_test{Chain{
-		Step{Drive(1)},
-		Executive{Teleop()}
-	}};
-
-	Executive drive_straight_test{Chain{
-		Step{Navx_drive_straight{12.0}},
-		Executive{Teleop()}
-	}};
-	
-	Executive rotate90_test{Chain{
-		Step{Navx_rotate{90.0}},
-		Executive{Teleop()}
-	}};
-	
-	Executive rotaten90_test{Chain{
-		Step{Navx_rotate{90.0}},
-		Executive{Teleop()}
-	}};
-
-	Executive combo_test{Chain{
-		vector<Step>{
-			Step{Navx_drive_straight{120.0}},
-			Step{Wait{2.0}},
-			Step{Navx_rotate{-90.0}},
-			Step{Wait{2.0}},
-			Step{Navx_drive_straight{132.0}},
-			Step{Wait{2.0}},
-			Step{Navx_rotate{-90.0}},
-			Step{Wait{2.0}},
-			Step{Navx_drive_straight{120.0}},
-			Step{Wait{2.0}},
-			Step{Navx_rotate{-90.0}},
-			Step{Wait{2.0}},
-			Step{Navx_drive_straight{132.0}},
-			Step{Wait{2.0}},
-			Step{Navx_rotate{-90.0}}
-		},
-		Executive{Teleop()}
-	}};
-
-	Executive same_switch{Chain{
-		Step{Navx_drive_straight{128.0}},
-		Executive{Teleop()}
-	}};
-
-	Executive rotate_test{Chain{
-		vector<Step>{
-			Step{Navx_rotate{-90.0}},
-			Step{Wait{2.0}},
-			Step{Navx_rotate{90.0}},
-			Step{Wait{2.0}},
-		},
-		Executive{Teleop()}
-	}};
-	
-
-	Executive opposite_switch{Chain{
-		vector<Step>{
-			Step{Navx_drive_straight{52.0}},
-			Step{Wait{1.0}},
-			Step{Navx_rotate{-90.0}},
-			Step{Wait{1.0}},
-			Step{Navx_drive_straight{100.0}},
-			Step{Wait{1.0}},
-			Step{Navx_rotate{90.0}},
-			Step{Wait{1.0}},
-			Step{Navx_drive_straight{56}}
-		},
-		Executive{Teleop()}
-	}};
-
-	Executive spin_test{Chain{
-		Step{Spin{0.2, -0.2}},
-		Executive{Teleop()}
-	}};
-
-	return rotate_test ;
-
-	/*
-	
-	if(!info.panel.in_use){
-		return auto_null;//Do nothing during autonomous mode if no panel exists
+#elif AUTOMODE_TEST == 1
+    //
+    // AUTOMODE_TEST == 1, drive straight 12 inches
+    //
+    logger.startMessage(messageLogger::messageType::info) ;
+    logger << "get_auto_mode - AUTOMODE_TEST == 1, drive 12 inches" ;
+    logger.endMessage() ;
+    
+    Executive auto_program
+    {
+	Chain
+	{
+	    Step
+	    {
+		Navx_drive_straight{12.0}
+	    },
+	    Executive
+	    {
+		Teleop()
+	    }
 	}
+    } ;
 
-	switch(info.panel.auto_select){
-		case 0: 
-			return auto_null;
-		case 1: 
-		case 2: 
-		case 3:
-		case 4: 
-		case 5: 
-		case 6:
-		case 7: 
-		case 8: 
-		case 9: 
-		case 10:
-		case 11:
-		case 12:
-		case 13:
-		case 14:
-		case 15:
-		case 16:
-		case 17:
-		case 18:
-		case 19:
-		default:
-			return auto_null;
+#elif AUTOMODE_TEST == 2
+    //
+    // AUTOMODE_TEST == 2, drive straight 10 feet
+    //
+    logger.startMessage(messageLogger::messageType::info) ;
+    logger << "get_auto_mode - AUTOMODE_TEST == 2, drive 10 ft" ;
+    logger.endMessage() ;
+    
+    Executive auto_program
+    {
+	Chain
+	{
+	    Step
+	    {
+		Navx_drive_straight{120.0}
+	    },
+	    Executive
+	    {
+		Teleop()
+	    }
 	}
-	*/
+    } ;
+
+#elif AUTOMODE_TEST == 3
+    //
+    // AUTOMODE_TEST = 3, rotate 90 degrees
+    //
+    logger.startMessage(messageLogger::messageType::info) ;
+    logger << "get_auto_mode - AUTOMODE_TEST == 3, rotate 90" ;
+    logger.endMessage() ;
+    
+    Executive auto_program
+    {
+	Chain
+	{
+	    Step
+	    {
+		Navx_rotate{90.0}
+	    },
+	    Executive
+	    {
+		Teleop()
+	    }
+	}
+    };
+
+#elif AUTOMODE_TEST == 4
+    //
+    // AUTOMODE_TEST = 4, rotate -90 degrees
+    //
+    logger.startMessage(messageLogger::messageType::info) ;
+    logger << "get_auto_mode - AUTOMODE_TEST == 4, rotate -90" ;
+    logger.endMessage() ;
+    
+    Executive auto_program
+    {
+	Chain
+	{
+	    Step
+	    {
+		Navx_rotate{-90.0}
+	    },
+	    Executive
+	    {
+		Teleop()
+	    }
+	}
+    };
+#elif AUTOMODE_TEST == 5
+    //
+    // AUTOMODE_TEST = 5, rotate 90 degress, wait 2 seconds, rotate -90 degrees
+    //
+    logger.startMessage(messageLogger::messageType::info) ;
+    logger << "get_auto_mode - AUTOMODE_TEST == 5, rotate 90, wait 2 secs, rotate -90" ;
+    logger.endMessage() ;
+    
+    Executive auto_program
+    {
+	Chain
+	{	
+	    vector<Step>
+	    {
+		Step
+		{
+		    Navx_rotate{-90.0}
+		},
+		Step
+		{
+		    Wait{2.0}
+		},
+		Step
+		{
+		    Navx_rotate{90.0}
+		},
+		Step
+		{
+		    Wait{2.0}
+		},
+	    },
+	    Executive
+	    {
+		Teleop()
+	    }
+	}
+    };
+    
+#elif AUTOMODE_TEST == 20
+    //
+    // AUTOMODE_TEST = 20, run the auto program for switch same side
+    //
+    logger.startMessage(messageLogger::messageType::info) ;
+    logger << "get_auto_mode - AUTOMODE_TEST == 20, switch same side" ;
+    logger.endMessage() ;
+    
+    Executive auto_program = same_switch ;
+	
+#elif AUTOMODE_TEST == 21
+    //
+    // AUTOMODE_TEST = 21, run the auto program for switch same side
+    //
+    logger.startMessage(messageLogger::messageType::info) ;
+    logger << "get_auto_mode - AUTOMODE_TEST == 21, switch opposite side" ;
+    logger.endMessage() ;
+    
+    Executive auto_program = opposide_switch ;
+	
+#else
+    //
+    // If AUTOMODE_TEST was not defined, we revert to the default behavior which is
+    // to run the with the panel
+    //
+    
+    logger.startMessage(messageLogger::messageType::info) ;
+    logger << "get_auto_mode - competition mode, selecting auto mode based on switch" ;
+    logger.endMessage() ;
+	
+    if(!info.panel.in_use) {
+	//
+	// No panel was detected.  May be a problem with the driver station, log this fact
+	// and do nothing during autonomous
+	//
+	logger.startMessage(messageLogger::messageType::error) ;
+	logger << "get_auto_mode - no panel detected, defaulting to null auto program" ;
+	logger.endMessage() ;
+	
+	return auto_null;
+    }
+
+    logger.startMessage(messageLogger::messageType::error) ;
+    logger << "get_auto_mode - panel value is " << info.panel.auto_select ;
+    logger.endMessage() ;
+    
+    switch(info.panel.auto_select){
+    case 0: 
+	return auto_null;
+    case 1:
+    case 2: 
+    case 3:
+    case 4: 
+    case 5: 
+    case 6:
+    case 7: 
+    case 8: 
+    case 9: 
+    case 10:
+    case 11:
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+    default:
+	return auto_null;
+    }
+#endif
+	
+    return auto_program ;
 }
 
 Executive Autonomous::next_mode(Next_mode_info info){
-	static const Time DELAY = 0.0;//seconds, TODO: base it off of the dial on the OI? 
-	if(!info.autonomous) return Executive{Teleop()};
-	if(info.since_switch > DELAY) return get_auto_mode(info);
-	return Executive{Autonomous()};
+    static const Time DELAY = 0.0;//seconds, TODO: base it off of the dial on the OI? 
+    if(!info.autonomous) return Executive{Teleop()};
+    if(info.since_switch > DELAY) return get_auto_mode(info);
+    return Executive{Autonomous()};
 }
 
 Toplevel::Goal Autonomous::run(Run_info){	
-	return {};//nothing, just waits
+    return {};//nothing, just waits
 }
 
 bool Autonomous::operator<(Autonomous const&)const{ return 0; }
@@ -171,14 +356,14 @@ bool Autonomous::operator<(Autonomous const&)const{ return 0; }
 bool Autonomous::operator==(Autonomous const&)const{ return 1; }
 
 void Autonomous::display(ostream& o)const{
-	o<<"Autonomous()";
+    o<<"Autonomous()";
 }
 
 
 #ifdef AUTONOMOUS_TEST
 #include "test.h"
 int main(){
-	Autonomous a;
-	test_executive(a);
+    Autonomous a;
+    test_executive(a);
 }
 #endif 
