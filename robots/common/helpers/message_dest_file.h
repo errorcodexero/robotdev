3dbd47f50e71d23585562491e92f1ebe6e57bc38
Butch Griffin <butchg@comcast.net>
2018-02-19 15:00:10 -0800

    flush output to file

--- robots/common/helpers/message_dest_file.h
+++ robots/common/helpers/message_dest_file.h
@@ -9,34 +9,34 @@ class messageDestFile : public messageLoggerDest
 public:
     messageDestFile()
     {
-	m_strm_p = nullptr ;
+		m_strm_p = nullptr ;
     }
     
     messageDestFile(const std::string &name)
     {
-	setFile(name) ;
+		setFile(name) ;
     }
     
     virtual ~messageDestFile()
     {
-	if (m_strm_p != nullptr)
-	    delete m_strm_p ;
+		if (m_strm_p != nullptr)
+			delete m_strm_p ;
     }
 
     void setFile(const std::string &name)
     {
-	m_strm_p = new std::ofstream(name) ;
-	if (!m_strm_p->is_open())
-	{
-	    std::cerr << "counld not open log file '" << name << "' for writing" << std::endl ;
-	    m_strm_p = nullptr ;
-	}
+		m_strm_p = new std::ofstream(name) ;
+		if (!m_strm_p->is_open())
+		{
+			std::cerr << "counld not open log file '" << name << "' for writing" << std::endl ;
+			m_strm_p = nullptr ;
+		}
     }
 
     virtual void displayMessage(const messageLogger::messageType &type, uint64_t subs, const std::string &msg)
     {
-	if (m_strm_p != nullptr)
-	    (*m_strm_p) << msg << std::endl ;
+		if (m_strm_p != nullptr)
+			(*m_strm_p) << msg << std::endl << std::flush ;
     }
 
 private:
