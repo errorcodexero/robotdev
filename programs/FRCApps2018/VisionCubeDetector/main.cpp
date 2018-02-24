35e473a326b7f7f5d3beec3ef0ed91db55d8f180
Mark Kassab <mark.a.kassab@gmail.com>
2018-02-24 22:45:06 +0000

    Vision: Initial crude merge of video stream from file vs. camera. Were 2 separate versions of main.cpp in Vi's repo.

--- programs/FRCApps2018/VisionCubeDetector/main.cpp
+++ programs/FRCApps2018/VisionCubeDetector/main.cpp
@@ -1,11 +1,21 @@
 #include "Cube.h"
 #include <iostream>
 
+// Added from branch for streaming from camera instead of file.  Most likely not needed, to be cleaned up.
+//#include <cstdlib>
+#include <libv4l2.h>
+#include <linux/videodev2.h>
+//#include <sys/ioctl.h>
+#include <fcntl.h>
+//#include <sstream>
+
 
 int main(int argc, char **argv) {
     std::string params_file(argv[1]);
     paramsInput params;
     params.readFile(params_file);
+
+#if 0  // Stream from file    
     //auto debug = params.getValue("CAMERA_EXPOSURE", -9001);
     std::string video_file(params.getString("FILENAME", ""));
     bool playVideo = true;
@@ -47,9 +57,46 @@ int main(int argc, char **argv) {
       }
       capture.release();
     }
-	
 
+#else // Stream from camera
+      double exposure = params.getValue("CAMERA_EXPOSURE",-9001);
+      std::stringstream ss;
+      ss << "/dev/video" << std::to_string(int(params.getValue("CAMID", -9001)));
+      int descriptor = v4l2_open(ss.str().c_str(), O_RDWR);
+      v4l2_control c;
+      c.id = V4L2_CID_EXPOSURE_AUTO;
+      c.value = int(params.getValue("CAMERA_EXPOSURE_AUTO", -9001));
+      if(v4l2_ioctl(descriptor, VIDIOC_S_CTRL, &c) == 0)
+	std::cout << "success";
+      /*c.id = V4L2_CID_EXPOSURE_AUTO_PRIORITY;
+  c.value = 0;
+  if (v4l2_ioctl(descriptor, VIDIOC_S_CTRL, &c) == 0)
+  std::cout << "success";*/
 
+      c.id = V4L2_CID_EXPOSURE_ABSOLUTE;
+      c.value = exposure;
+      if (v4l2_ioctl(descriptor, VIDIOC_S_CTRL, &c) == 0)
+	std::cout << "success";
+      cv::VideoCapture capture(params.getValue("CAMID", 0));
+      while (1) {
+	if (!capture.isOpened()) {
+	  std::cout << "ERROR ACQUIRING VIDEO FEED\n";
+	  getchar();
+	  return -1;
+	}
+	cv::Mat frame;
+	//read first frame
+	capture >> frame;
+	Cube cube(frame, params);
+	cube.getPosition(Cube::detectionMode::BLOB);
+	cv::imshow("Cube", cube.showFrame());
+	//cv::imshow("Camera", frame);
+	//cv::imshow("Video", frame);
+	cv::waitKey(10);
+      };
+      capture.release();
+      
+#endif
   
     return 0;
 }
