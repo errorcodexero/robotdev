c9ece2cbf1182e5d395578f421c6b0c192ad500b
Matthew Macovsky <macovsky.m@gmail.com>
2018-02-24 14:58:33 -0800

    Change drop collector mode to slowly run intake out

--- robots/claymore/executive/teleop.cpp
+++ robots/claymore/executive/teleop.cpp
@@ -50,7 +50,7 @@ Executive Teleop::next_mode(Next_mode_info info) {
 
 IMPL_STRUCT(Teleop::Teleop,TELEOP_ITEMS)
 
-Teleop::Teleop():lifter_goal(Lifter::Goal::stop()),wings_goal(Wings::Goal::LOCKED),collector_mode(Collector_mode::DO_NOTHING),started_eject_with_cube(false),high_gear(false){}
+Teleop::Teleop():lifter_goal(Lifter::Goal::stop()),wings_goal(Wings::Goal::LOCKED),collector_mode(Collector_mode::DO_NOTHING),started_intake_with_cube(false),high_gear(false){}
 
 Toplevel::Goal Teleop::run(Run_info info) {
 	messageLogger &logger = messageLogger::get();
@@ -122,15 +122,17 @@ Toplevel::Goal Teleop::run(Run_info info) {
 			if(info.status.grabber.has_cube) collector_mode = Collector_mode::GRABBING;
 			break;
 		case Collector_mode::EJECT:
-			goals.grabber = Grabber::Goal::go_to_preset(GrabberController::Preset::CLOSED);
-			goals.intake = Intake::Goal::out();
-			eject_timer.update(info.in.now, info.in.robot_mode.enabled);
-			if((started_eject_with_cube && !info.status.grabber.has_cube) || eject_timer.done())
-				collector_mode = Collector_mode::DO_NOTHING;
-			break;
 		case Collector_mode::DROP:
-			goals.grabber = Grabber::Goal::go_to_preset(GrabberController::Preset::OPEN);
-			goals.intake = Intake::Goal::off();
+			if(collector_mode == Collector_mode::EJECT) {
+				goals.grabber = Grabber::Goal::go_to_preset(GrabberController::Preset::CLOSED);
+				goals.intake = Intake::Goal::out();
+			} else {
+				goals.grabber = Grabber::Goal::go_to_preset(GrabberController::Preset::OPEN);
+				goals.intake = Intake::Goal::out(0.2);
+			}
+			intake_timer.update(info.in.now, info.in.robot_mode.enabled);
+			if((started_intake_with_cube && !info.status.grabber.has_cube) || intake_timer.done())
+				collector_mode = Collector_mode::DO_NOTHING;
 			break;
 		default: assert(0);
 	}
@@ -192,10 +194,14 @@ Toplevel::Goal Teleop::run(Run_info info) {
 	if(info.panel.collect_closed) collector_mode = Collector_mode::COLLECT_CLOSED;
 	if(info.panel.eject) {
 		collector_mode = Collector_mode::EJECT;
-		started_eject_with_cube = info.status.grabber.has_cube;
-		eject_timer.set(1);
+		started_intake_with_cube = info.status.grabber.has_cube;
+		intake_timer.set(1.0);
 	}
-	if(info.panel.drop) collector_mode = Collector_mode::DROP;
+	if(info.panel.drop) {
+		collector_mode = Collector_mode::DROP;
+		started_intake_with_cube = info.status.grabber.has_cube;
+		intake_timer.set(0.5);
+	}
 
 	if(calibrate_trigger(info.panel.calibrate)) {
 		Lifter::lifter_controller.setCalibrate(true);
