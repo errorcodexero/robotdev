4628ef73a7dbdc7cd062dd2a4842add298e0379f
Matthew Macovsky <macovsky.m@gmail.com>
2018-02-24 12:26:56 -0800

    Add Collect step

--- robots/claymore/executive/step.cpp
+++ robots/claymore/executive/step.cpp
@@ -712,6 +712,46 @@ bool Grabber_to_preset::operator==(Grabber_to_preset const& b)const{
 }
 
 //
+// Collect: Put the grabber into collecting mode until a cube is collected
+//
+
+Collect::Collect(double time):time(time),init(false){}
+
+Step::Status Collect::done(Next_mode_info info){
+    Step::Status ret = info.status.grabber.has_cube ? Step::Status::FINISHED_SUCCESS : Step::Status::UNFINISHED;
+    if (ret == Step::Status::FINISHED_SUCCESS)
+    {
+	messageLogger &logger = messageLogger::get() ;
+	logger.startMessage(messageLogger::messageType::debug, SUBSYSTEM_AUTONOMOUS) ;
+	logger << "Collect complete" ;
+	logger.endMessage() ;
+    }
+    return ret ;
+	
+}
+
+Toplevel::Goal Collect::run(Run_info info){
+    return run(info,{});
+}
+
+Toplevel::Goal Collect::run(Run_info info,Toplevel::Goal goals){
+    if(!init) {
+	Grabber::grabber_controller.moveToAngle(GrabberController::Preset::OPEN, time);
+	init = false;
+    }
+    goals.grabber = Grabber::Goal::go_to_preset(GrabberController::Preset::OPEN);
+    return goals;
+}
+
+unique_ptr<Step_impl> Collect::clone()const{
+    return unique_ptr<Step_impl>(new Collect(*this));
+}
+
+bool Collect::operator==(Collect const& b)const{
+    return time == b.time && init == b.init;
+}
+
+//
 // Eject: Eject a cube
 //
 
