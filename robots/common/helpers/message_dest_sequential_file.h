6bce0ec65ec8d63fe322569a6df9cd8b5257f19c
Vi Vuong <vuongkyvi@gmail.com>
2018-04-03 14:33:38 -0700

    Switched from ifstream::isopen to stat for checking file existence

--- robots/common/helpers/message_dest_sequential_file.h
+++ robots/common/helpers/message_dest_sequential_file.h
@@ -2,21 +2,22 @@
 #include "message_dest_file.h"
 #include <fstream>
 #include <string>
+#include "unistd.h"
+#include <sys/types.h>
+#include <sys/stat.h>
 
 class messageDestSequentialFile : public messageDestFile {
 public:
 	/// \brief Sequential file naming with a given prefix and extension
 	messageDestSequentialFile(const std::string& prefix, const std::string& ext) {
-		std::ifstream f;
 		unsigned long int i = 0;
 		std::string filename;
 		bool opened;
 		do {
 			filename = prefix + std::to_string(i) + ext;
 			i++;
-			f.open(filename);
-			opened = f.is_open();
-			f.close();
+			struct stat buffer;
+			opened = stat(filename.c_str(), &buffer);
 		} while (opened);
 		setFile(filename);
 	}
