72c81faa269d3f9298938eed4108699057688ea7
Jack (Butch) Griffin <butch@comcast.net>
2018-04-23 14:07:40 -0700

    fixing issues related to pathfinder

--- robots/sim/simrobot/LineArcLinePathContainer.cpp
+++ robots/sim/simrobot/LineArcLinePathContainer.cpp
@@ -27,11 +27,11 @@ std::shared_ptr<xero::pathfinder::Path> LineArcLinePathContainer::buildPath(doub
 	points.push_back(Waypoint(-7.02944, 16.97056, deg2rad(135), 24.0));
 	points.push_back(Waypoint(-24.00, 24.0, deg2rad(180.0), 24.0));
 #else
-	points.push_back(Waypoint(120.775, 107.996, 0.0, 80.0));
-	points.push_back(Waypoint(108.775, 87.181, 10.0, 80.0));
-	points.push_back(Waypoint(143.446, 61.145, 30.0, 80.0));
-	points.push_back(Waypoint(115.161, 32.861, 10.0, 80.0));
-	points.push_back(Waypoint(84.5, 26.861, 0.0, 80.0));
+	points.push_back(Waypoint(120.775, 107.966,  0.0, 80.0));
+	points.push_back(Waypoint(108.775,  87.181, 10.0, 80.0));
+	points.push_back(Waypoint(143.44559465474475, 61.14542326662129, 30.0, 80.0));
+	points.push_back(Waypoint(115.16132340728284, 32.8611520191594, 10.0, 80.0));
+	points.push_back(Waypoint( 84.500,  26.861,  0.0, 80.0));
 #endif
 
 	return xero::pathfinder::PathBuilder::buildPath(points, maxaccel);
@@ -39,7 +39,7 @@ std::shared_ptr<xero::pathfinder::Path> LineArcLinePathContainer::buildPath(doub
 
 xero::math::PositionCS LineArcLinePathContainer::getStartPose()
 {
-	PositionCS ret(Position(0.0, 0.0), Rotation::fromDegrees(90.0));
+	PositionCS ret(Position(112, 115), Rotation::fromDegrees(0.0));
 	return ret;
 }
 
